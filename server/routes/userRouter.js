3dfdb87dd50b6dfbfbddb5928cdb3f856c54f0e8
DashinIN <green19grant@yandex.ru>
2023-03-26 16:26:45 +0300

    home merge

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,7 +1,7 @@
 const Router = require('express')
 const userController = require('../controllers/userController')
 const router = new Router()
-const UserController = require('../controllers/userController')
+
 
 router.post('/registration', userController.registration)
 router.post('/login', userController.login)
